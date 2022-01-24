cca14e6417d4fa8951e33c27f34c5a4c5f0e295d
hrkmay <hrkmay8492@gmail.com>
2022-01-25 00:11:54 +0900

    パスワードは半角英数字混合である

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          validates :nickname,           presence: true
-         validates :email,              presence: true
-         validates :encrypted_password, presence: true
+         validates :email, uniqueness: true, presence: true
+         VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+         validates :encrypted_password, presence: true, confirmation: true, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }
          validates :first_name,         presence: true
          validates :last_name,          presence: true
          validates :first_kana_name,    presence: true
