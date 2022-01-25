1a88794a7eecd1b154e09a97a1c0f113993912f0
hrkmay <hrkmay8492@gmail.com>
2022-01-25 23:03:29 +0900

    バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          validates :nickname,           presence: true
-         validates :email, uniqueness: true, presence: true
-         VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-         validates :encrypted_password, presence: true, confirmation: true, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }
+         validates :email,              presence: true
+         validates :encrypted_password, presence: true
          validates :first_name,         presence: true
          validates :last_name,          presence: true
          validates :first_kana_name,    presence: true
