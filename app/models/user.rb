9f53148a6bb27cc6053bcd2f5f840c048132c78c
hrkmay <hrkmay8492@gmail.com>
2022-01-26 17:46:39 +0900

    モデル単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          validates :nickname,           presence: true
-         validates :email,              presence: true
-         validates :encrypted_password, presence: true
-         validates :first_name,         presence: true
-         validates :last_name,          presence: true
-         validates :first_kana_name,    presence: true
-         validates :last_kana_name,     presence: true
+         validates :email, uniqueness: true, presence: true
+         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+         validates :password, presence: true, confirmation: true, length: { minimum: 6 }, format: { with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' }
+         validates :first_name,         presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
+         validates :last_name,          presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
+         validates :first_kana_name,    presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
+         validates :last_kana_name,     presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
          validates :birth,              presence: true
 end
