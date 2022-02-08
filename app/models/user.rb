a6fc06b19ab76ef36a2dfbcaec4a55bce2166eb3
hrkmay <hrkmay8492@gmail.com>
2022-02-09 00:23:31 +0900

    テーブル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
-class User < ApplicationRecord
+class ApplicationRecord < ActiveRecord::Base
+  self.abstract_class = true
+end
+
+class User < ApplicationRecord 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_07_121033) do
+ActiveRecord::Schema.define(version: 2022_02_08_150513) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "items_name", null: false
