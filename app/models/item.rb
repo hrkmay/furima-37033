e5ed25ac60790515381cee111c0302c3e2f0fc10
hrkmay <hrkmay8492@gmail.com>
2022-02-10 23:23:38 +0900

    出品ページ入力欄作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,8 +7,8 @@ class Item < ApplicationRecord
   
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
-  
-
-   
-
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_price
+  belongs_to_active_hash :place
+  belongs_to_active_hash :day_to_ship
 end
