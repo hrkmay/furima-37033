e36eb02c0cafe1cd49c4d90ac05dbc90133b43b8
hrkmay <hrkmay8492@gmail.com>
2022-02-12 23:52:24 +0900

    データベースに保存する

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,19 @@
 class Item < ApplicationRecord
-  validates :title, :text, presence: true
   
+  validates :image,                presence: true
+  validates :items_name,           presence: true
+  validates :info,                 presence: true
+  validates :category_id,          presence: true
+  validates :condition_id,         presence: true
+  validates :shipping_price_id,    presence: true
+  validates :place_id,             presence: true
+  validates :day_to_ship_id,       presence: true
+  validates :price, numericality: { in: 300..9999999 }, presence: true
+
 
   belongs_to :user
   has_one :record
+  has_one_attached :image
   
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
