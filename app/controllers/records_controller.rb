74c283c32a2c41b5a9be3ad2d069f2e5507bf426
hrkmay <hrkmay8492@gmail.com>
2022-03-20 00:53:03 +0900

    仮カラムの消去

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,6 +1,7 @@
 class RecordsController < ApplicationController
   before_action :authenticate_user!, only: [:index]
-
+  before_action :set_record, only: [:index, :create]
+  before_action :move_to_index, only: [:index, :create]
   def index
     @record_address = RecordAddress.new
     @item = Item.find(params[:item_id])
@@ -24,8 +25,17 @@ class RecordsController < ApplicationController
   end
 
   private
-  def record_params
-    params.require(:record_address).permit(:token, :hoge, :building_name, :postial_code, :place_id, :city, :house_number, :phone_number).merge(token: params[:token], item_id: params[:item_id], user_id: current_user.id)
+  def set_record
+    @item = Item.find(params[:item_id])
   end
 
+  def record_params
+    params.require(:record_address).permit(:token, :building_name, :postial_code, :place_id, :city, :house_number, :phone_number).merge(token: params[:token], item_id: params[:item_id], user_id: current_user.id)
+  end
+
+  def move_to_index
+    if current_user.id == @item.user_id || @item.record != nil
+      redirect_to root_path
+    end
+  end
 end
--- app/models/record_address.rb
+++ app/models/record_address.rb
@@ -1,6 +1,6 @@
 class RecordAddress
   include ActiveModel::Model
-  attr_accessor :token, :user_id, :item_id, :postial_code, :place_id, :city, :house_number, :building_name, :phone_number, :record, :hoge
+  attr_accessor :token, :user_id, :item_id, :postial_code, :place_id, :city, :house_number, :building_name, :phone_number, :record
 
   with_options presence: true do
    validates :postial_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
